abe497bc77b8b414c28340aafac964d7a0af9586
mourasuporte24 <mourasuporte24@gmail.com>
2020-10-27 20:34:44 -0300

    For em 4 Situações

--- src/For.java
+++
@@ -1,8 +0,0 @@
-
-public class For {
-
-	public static void main(String[] args) {
-		System.out.println("Vamos lá !!!!");
-	}
-
-}
---
+++ src/Use_For.java
@@ -0,0 +1,11 @@
+
+public class Use_For {
+
+	public static void main(String[] args) {
+		for(int i = 4 ; i >= 0;i--) {
+			System.out.println("O valor de I é :" + i);
+		}
+
+	}
+
+}
