1740693177ad3eac4c942277fa615ecc17240ce6
mourasuporte24 <mourasuporte24@gmail.com>
2020-10-28 08:07:46 -0300

    Equivalencia entre Temperaturas

--- src/Use_For.java
+++ src/Use_For.java
@@ -1,11 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Use_For {
 
 	public static void main(String[] args) {
-		for(int i = 4 ; i >= 0;i--) {
-			System.out.println("O valor de I é :" + i);
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digte a Temperatura em Celsius");
+		double c = sc.nextDouble();
+		double f = 9*c /5 +32;
+		int i;
+		System.out.println("Valor em Fareniht equivale a :" + f);
+		System.out.println("-------------------------------------------");
+	
+		System.out.println("Voce deseja testar novamente S / N");
+		
+		while (i == s) {
+			case 1 :
+				i = s;
+				break;
+				
+		
+		default: "Erro";
+			break;
 		}
-
+		
+		
+		
+	
 	}
 
 }
